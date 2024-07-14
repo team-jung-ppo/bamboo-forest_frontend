7663ff90f4e866d6f021028eae5099e8d846b913
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-14 21:19:46 +0900

    ✨Feat : 메인 페이지 초기 빌드

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <>
+      <div className="logoimg">로고 이미지</div>
+      <div className="serviceInfo">대나무숲 서비스에 대한 설명</div>
+      <div className="batteryInfo">건전지 정보 ...</div>
+      <div className="concepts">
+        <div className="concept1">어린아이</div>
+        <div className="concept2">돌팔이 의사</div>
+        <div className="concept3">아저씨</div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
--- src/router.jsx
+++ src/router.jsx
@@ -1,26 +1,25 @@
-import {UnknownErrorBoundary} from "./errors/UnknownErrorBoundary.jsx";
-import { Outlet, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import { Loginpage } from "./components/Loginpage";
-import {ChattingPage} from "./components/ChattingPage/index.jsx";
+import { UnknownErrorBoundary } from './errors/UnknownErrorBoundary.jsx';
+import {
+	Outlet,
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from 'react-router-dom';
+import { Loginpage } from './components/Loginpage';
+import { ChattingPage } from './components/ChattingPage/index.jsx';
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/"
-      component={
-        <UnknownErrorBoundary>
-          <Outlet />
-        </UnknownErrorBoundary>
-      }
-    >
-      <Route
-        path='/login'
-        element={<Loginpage />}
-      />
-      <Route
-        path='/chatting'
-        element={<ChattingPage />}
-      />
-    </Route>,
-  ),
+	createRoutesFromElements(
+		<Route
+			path="/"
+			component={
+				<UnknownErrorBoundary>
+					<Outlet />
+				</UnknownErrorBoundary>
+			}
+		>
+			<Route path="/login" element={<Loginpage />} />
+			<Route path="/chatting" element={<ChattingPage />} />
+		</Route>,
+	),
 );
