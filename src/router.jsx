a9b4e9c815f2a0a149649e1a48afed2bbae647be
Hwangyongjin <dswvgw6412@naver.com>
2024-07-15 00:30:09 +0900

    :memo:Feat : 채팅 페이지 라우팅 적용

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import {UnknownErrorBoundary} from "./errors/UnknownErrorBoundary.jsx";
 import { Outlet, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { Loginpage } from "./components/Loginpage";
+import {ChattingPage} from "./components/ChattingPage/index.jsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,10 @@ export const router = createBrowserRouter(
       <Route
         path='/login'
         element={<Loginpage />}
+      />
+      <Route
+        path='/chatting'
+        element={<ChattingPage />}
       />
     </Route>,
   ),
