30db4e5ec96bf1bb4afa1b0b385d6757b8f283ad
Hwangyongjin <dswvgw6412@naver.com>
2024-07-14 20:12:15 +0900

    :sparkles:Feat : 카카오로그인 기능 추가

---
+++ src/hooks/auth/useKakaoOAuth.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+import { KAKAO_AUTH_URL, KAKAO_LOGOUT_URL } from '../../consts/kakao';
+
+export function useKakaoOAuth() {
+  const requestAuthorization = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
+  const logout = async () => {
+    const res = await axios.get(KAKAO_LOGOUT_URL, { withCredentials: true });
+    if (res.status === 200) {
+      window.location.reload();
+    }
+  };
+
+  return { requestAuthorization, logout };
+}
