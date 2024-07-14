4786bc162027d095f58d84ed4c6bf1b50b628374
Hwangyongjin <dswvgw6412@naver.com>
2024-07-14 16:55:02 +0900

    :memo:Feat : 로그인 버튼 관련 상수화

---
+++ src/consts/LoginBtn.js
@@ -0,0 +1,17 @@
+import KakaoSymbol from '../assets/kakaoLoginSymbol.png';
+import GithubSymbol from '../assets/githubLoginSymbol.png';
+
+export const LoginBtn = [
+  {
+    symbol: KakaoSymbol,
+    text: "카카오 로그인",
+    fontColor: 'black',
+    backgroundColor: '#FEE500',
+  },
+  {
+   symbol:  GithubSymbol,
+    text: "깃허브 로그인",
+    fontColor: 'white',
+    backgroundColor: 'black',
+  }
+]
