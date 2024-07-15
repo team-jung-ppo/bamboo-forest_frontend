ebb384bfe62927c9c3ef4c3c2e8cb4d67d0c35ec
Hwangyongjin <dswvgw6412@naver.com>
2024-07-15 16:32:35 +0900

    :sparkles:Modify : 사이드바 토글기능 보완

--- src/components/common/SideBar/SideBar.jsx
+++ src/components/common/SideBar/SideBar.jsx
@@ -2,21 +2,22 @@ import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArro
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import styles from "./sideBar.module.css";
 import {useState} from "react";
+import {Logo} from "../Logo/Logo.jsx";
 
-export function SideBar({ open, setOpen }) {
+export function SideBar({ open, onToggleSideBar }) {
   return (
-    <div className={open ? styles.block : styles.nonBlock}>
+    <div className={!open ? styles.block : open === '_true' ? styles.block_true : styles.block_false}>
       <div className={styles.arrowBlock}>
-        {open ?
+        {open === '_true' || !open ?
           <KeyboardDoubleArrowLeftIcon
             className={styles.arrow}
             style={{ fontSize: "36px" }}
-            onClick={() => setOpen(!open)}
+            onClick={onToggleSideBar}
           /> :
           <KeyboardDoubleArrowRightIcon
             className={styles.arrow}
             style={{ fontSize: "36px" }}
-            onClick={() => setOpen(!open)}
+            onClick={onToggleSideBar}
           />
         }
       </div>
