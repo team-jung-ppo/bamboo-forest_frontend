819df6c9068fa23cfaebd2d6c4093d69fbb16daa
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-16 12:59:02 +0900

    ✨ Feat : 건전지 컴포넌트 구현

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Logo } from '../common/Logo/Logo.jsx';
 import { About } from '../common/About/About.jsx';
+import { Batteries } from '../common/Batteries/Batteries.jsx';
 
 const MainPage = () => {
-  return (
-    <>
-      <Logo />
-      <About />
-      <div className="batteryInfo">건전지 정보 ...</div>
-      <div className="concepts">
-        <div className="concept1">어린아이</div>
-        <div className="concept2">돌팔이 의사</div>
-        <div className="concept3">아저씨</div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Batteries />
+			<Logo />
+			<About />
+			<div className="concepts">
+				<div className="concept1">어린아이</div>
+				<div className="concept2">돌팔이 의사</div>
+				<div className="concept3">아저씨</div>
+			</div>
+		</>
+	);
 };
 
 export default MainPage;
---
+++ src/components/common/Batteries/Batteries.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Batteries.css';
+
+export function Batteries() {
+	const [battery, setBattery] = useState(0);
+
+	useEffect(() => {
+		axios.get('https://{{url}}/api/batteries/info').then((responce) => {
+			setBattery(responce.data);
+		});
+	});
+	return (
+		<>
+			<div className="batteriesInfo">
+				<div className="batteriesCnt">🔋 현재 보유한 건전지 {battery}개</div>
+				<button className="chargeBtn">건전지 충전하기</button>
+			</div>
+		</>
+	);
+}
