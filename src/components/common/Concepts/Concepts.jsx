ddfc5f7cf9cd9b4622dd3327e35d6790d0064950
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-19 19:35:41 +0900

    ✨ Add : Concepts 페이지에 건전지 추가

--- src/components/common/Concepts/Concepts.jsx
+++ src/components/common/Concepts/Concepts.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Concepts.css';
 
 function Concepts(props) {
+	const [battery, setBattery] = useState(0);
+	const [modal, setModal] = useState('false');
+
+	useEffect(() => {
+		axios.get('https://{{url}}/api/batteries/info').then((responce) => {
+			setBattery(responce.data);
+		});
+	});
+
+	const checkAvailable = () => {
+		if (battery < props.cost) setModal(true);
+	};
 	return (
 		<div className="container">
 			<img src={props.img} alt="" className="conceptImg" />
 			<div className="conceptName">{props.name}</div>
 			<div className="features">{props.features}</div>
 			<div className="cost">{props.cost} 건전지 필요</div>
-			<button className="chooseBtn">선택하기</button>
+			<button onClick={checkAvailable} className="chooseBtn">
+				선택하기
+			</button>
 		</div>
 	);
 }
