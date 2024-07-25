bc611882de9aed88f90a92583751afe178ad8897
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-25 10:18:14 +0900

    🎨 Modify : MainPage map 함수 적용

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,30 +6,41 @@ import Concepts from '../common/Concepts/Concepts.jsx';
 import styles from './mainPage.module.css';
 
 const MainPage = () => {
+	const conceptsData = [
+		{
+			img: 'src/assets/bambooForestLogo.png',
+			name: '어린아이',
+			features: '명량함',
+			cost: '200',
+		},
+		{
+			img: 'src/assets/bambooForestLogo.png',
+			name: '돌팔이 의사',
+			features: '엉뚱함',
+			cost: '200',
+		},
+		{
+			img: 'src/assets/bambooForestLogo.png',
+			name: '아저씨',
+			features: '잔소리',
+			cost: '0',
+		},
+	];
 	return (
 		<>
 			<Batteries />
 			<Logo />
 			<About />
 			<div className={styles.concepts}>
-				<Concepts
-					img="src/assets/bambooForestLogo.png"
-					name="어린아이"
-					features="명량함"
-					cost="200"
-				/>
-				<Concepts
-					img="src/assets/bambooForestLogo.png"
-					name="돌팔이 의사"
-					features="엉뚱함"
-					cost="200"
-				/>
-				<Concepts
-					img="src/assets/bambooForestLogo.png"
-					name="아저씨"
-					features="잔소리"
-					cost="0"
-				/>
+				{conceptsData.map((concepts, index) => (
+					<Concepts
+						key={index}
+						img={concepts.img}
+						name={concepts.name}
+						features={concepts.features}
+						cost={concepts.cost}
+					/>
+				))}
 			</div>
 		</>
 	);
