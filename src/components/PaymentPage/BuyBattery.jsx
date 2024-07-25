d08af233beb0ad2acbcd800310da0d540503375b
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-25 10:18:38 +0900

    🎨 Modify : BuyBattery 컴포넌트 map 함수 적용

--- src/components/PaymentPage/BuyBattery.jsx
+++ src/components/PaymentPage/BuyBattery.jsx
@@ -3,6 +3,20 @@ import { Batteries } from '../common/Batteries/Batteries';
 import BuyBatteryComponent from '../common/Batteries/BuyBatteryComponent';
 
 function BuyBattery() {
+	const buyBatteryData = [
+		{
+			batterynum: '50',
+			cost: '1,000',
+		},
+		{
+			batterynum: '100',
+			cost: '1,700',
+		},
+		{
+			batterynum: '2,500',
+			cost: '2,500',
+		},
+	];
 	return (
 		<div id={styles.buybattery}>
 			<div className={styles.batteriesContainer}>
@@ -13,9 +27,9 @@ function BuyBattery() {
 				<h3>추가할 배터리 수를 선택하세요.</h3>
 			</div>
 			<div className={styles.buybatterylist}>
-				<BuyBatteryComponent batterynum="50" cost="1,000" />
-				<BuyBatteryComponent batterynum="100" cost="1,700" />
-				<BuyBatteryComponent batterynum="150" cost="2,500" />
+				{buyBatteryData.map((data, index) => (
+					<BuyBatteryComponent batterynum={data.batterynum} cost={data.cost} />
+				))}
 			</div>
 		</div>
 	);
