7addccb6d2de768432d9a504c8c9f4bfaff26129
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-26 10:46:18 +0900

    ✨ Feat : API 연동 및 Chatbot컴포넌트 사용 출력

--- src/components/PaymentPage/BuyChatbot.jsx
+++ src/components/PaymentPage/BuyChatbot.jsx
@@ -1,9 +1,37 @@
 import styles from './buyChatbot.module.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import ChatBotComponent from '../common/ChatBot/ChatBotComponent';
+import { Batteries } from '../common/Batteries/Batteries';
 
 function BuyChatbot() {
+	const chatbotData = [];
+	useEffect(() => {
+		axios.get('https://54.180.99.46:8080/api/chatbots').then((responce) => {
+			chatbotData.push(...responce);
+		});
+	});
 	return (
 		<div className={styles.container}>
-			<h2>Buy Chatbot Page.</h2>
+			<div id={styles.buybattery}>
+				<div className={styles.batteriesContainer}>
+					<Batteries />
+				</div>
+				<div className={styles.logoandtxt}>
+					<img src="src/assets/bambooForestLogo.png" alt="" />
+					<h3>구매할 챗봇을 선택하세요.</h3>
+				</div>
+				<div className={styles.buychatbotlist}>
+					{chatbotData.map((data, index) => (
+						<ChatBotComponent
+							imgUrl={data.imgUrl}
+							name={data.name}
+							description={data.description}
+							cost={'1000'}
+						/>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }
