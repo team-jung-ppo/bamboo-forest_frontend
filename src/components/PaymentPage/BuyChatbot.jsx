858573d400642956641919cc9255a5124aa96a04
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-26 09:49:58 +0900

    ✨ Feat : Chatbot 결제 페이지 추가

---
+++ src/components/PaymentPage/BuyChatbot.jsx
@@ -0,0 +1,11 @@
+import styles from './buyChatbot.module.css';
+
+function BuyChatbot() {
+	return (
+		<div className={styles.container}>
+			<h2>Buy Chatbot Page.</h2>
+		</div>
+	);
+}
+
+export default BuyChatbot;
