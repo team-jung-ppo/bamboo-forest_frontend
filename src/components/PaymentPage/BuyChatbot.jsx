47134710f1e0dbae906f6cdeec7016eb39075434
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-26 16:33:50 +0900

    ✨ Modify : api 연동 방식 수정, 챗봇 컴포넌트 중앙정렬

--- src/components/PaymentPage/BuyChatbot.jsx
+++ src/components/PaymentPage/BuyChatbot.jsx
@@ -3,34 +3,59 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import ChatBotComponent from '../common/ChatBot/ChatBotComponent';
 import { Batteries } from '../common/Batteries/Batteries';
+import Header from '../MainPage/Header';
+import client from '../../services/api/axios';
+import { getCookie } from '../../services/cookie';
+import { useState } from 'react';
 
 function BuyChatbot() {
-	const chatbotData = [];
+	const [chatbotData, setChatbotData] = useState([]);
+	const fetchChatbotInfo = async () => {
+		try {
+			const accessToken = getCookie('accessToken');
+			const response = await axios.get(
+				`${import.meta.env.VITE_WAS_URL}/api/chatbots`,
+				{
+					withCredentials: true,
+					headers: {
+						Authorization: `${accessToken}`,
+					},
+				},
+			);
+			const userData = response.data;
+			setChatbotData(userData);
+		} catch (e) {
+			if (axios.isAxiosError(e)) {
+				console.error('Axios error:', e.response?.data || e.message);
+			} else {
+				console.error('Unknown error:', e);
+			}
+		}
+	};
+
 	useEffect(() => {
-		axios.get('https://54.180.99.46:8080/api/chatbots').then((responce) => {
-			chatbotData.push(...responce);
-		});
-	});
+		fetchChatbotInfo();
+	}, []);
+
 	return (
 		<div className={styles.container}>
-			<div id={styles.buybattery}>
-				<div className={styles.batteriesContainer}>
-					<Batteries />
-				</div>
-				<div className={styles.logoandtxt}>
-					<img src="src/assets/bambooForestLogo.png" alt="" />
-					<h3>구매할 챗봇을 선택하세요.</h3>
-				</div>
-				<div className={styles.buychatbotlist}>
-					{chatbotData.map((data, index) => (
-						<ChatBotComponent
-							imgUrl={data.imgUrl}
-							name={data.name}
-							description={data.description}
-							cost={'1000'}
-						/>
-					))}
-				</div>
+			<div className={styles.batteriesContainer}>
+				<Header />
+				<Batteries />
+			</div>
+			<div className={styles.logoandtxt}>
+				<h3>구매할 챗봇을 선택하세요.</h3>
+			</div>
+			<div className={styles.buychatbotlist}>
+				{chatbotData.map((data, index) => (
+					<ChatBotComponent
+						key={index}
+						imgUrl={data.imgUrl}
+						name={data.name}
+						description={data.description}
+						cost={'1000'}
+					/>
+				))}
 			</div>
 		</div>
 	);
