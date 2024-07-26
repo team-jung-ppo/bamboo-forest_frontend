61b175a78dc2e14257d26c595562a1a9da709789
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-26 15:39:39 +0900

    ✨ Feat : Help 페이지 구성

--- src/components/HelpPage/HelpPage.jsx
+++ src/components/HelpPage/HelpPage.jsx
@@ -1,9 +1,12 @@
 import styles from './helppage.module.css';
+import Header from '../MainPage/Header';
 
 function HelpPage() {
 	return (
 		<div className={styles.container}>
+			<Header />
 			<h3>Help Page</h3>
+			<p>Help ............. Text ..........</p>
 		</div>
 	);
 }
