0c3e8ba1ca128bf4bd5ac5b25b3e5357b3b2b598
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-26 17:31:29 +0900

    ✨ Feat : UserInfo 페이지 작성

---
+++ src/components/UserInfoPage/UserInfo.jsx
@@ -0,0 +1,56 @@
+import { Batteries } from '../common/Batteries/Batteries';
+import Header from '../MainPage/Header';
+import styles from './userinfo.module.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { getCookie } from '../../services/cookie';
+import PayedListComponent from '../common/PayedList/PayedListComponent';
+
+function UserInfo() {
+	const [userinfo, setUserinfo] = useState([]);
+	const fetchUserInfo = async () => {
+		try {
+			const accessToken = getCookie('accessToken');
+			const response = await axios.get(
+				`${import.meta.env.VITE_WAS_URL}/api/members/profile`,
+				{
+					withCredentials: true,
+					headers: {
+						Authorization: `${accessToken}`,
+					},
+				},
+			);
+			const userData = response.data;
+			setUserinfo(userData);
+		} catch (e) {
+			if (axios.isAxiosError(e)) {
+				console.error('Axios error:', e.response?.data || e.message);
+			} else {
+				console.error('Unknown error:', e);
+			}
+		}
+	};
+
+	useEffect(() => {
+		fetchUserInfo();
+	}, []);
+	return (
+		<div className={styles.container}>
+			<div className={styles.topcontainer}>
+				<Header />
+				<Batteries />
+			</div>
+			<div className={styles.name}>{userinfo.username} 님 환영합니다!</div>
+			<div className={styles.batteryInfo}>
+				<h2>배터리 결제내역</h2>
+				<PayedListComponent />
+			</div>
+			<div className={styles.chatbotInfo}>
+				<h2>챗봇 정보</h2>
+				<h2>챗봇 구매내역</h2>
+			</div>
+		</div>
+	);
+}
+
+export default UserInfo;
