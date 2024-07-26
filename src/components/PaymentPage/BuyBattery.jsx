53fce67e47585d9d73cc18fe59a2c431237493a5
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-26 15:14:05 +0900

    💄  Modify : BuyBattery 페이지 스타일 수정

--- src/components/PaymentPage/BuyBattery.jsx
+++ src/components/PaymentPage/BuyBattery.jsx
@@ -1,6 +1,7 @@
 import styles from './buyBattery.module.css';
 import { Batteries } from '../common/Batteries/Batteries';
 import BuyBatteryComponent from '../common/Batteries/BuyBatteryComponent';
+import Header from '../MainPage/Header';
 
 function BuyBattery() {
 	const buyBatteryData = [
@@ -19,11 +20,11 @@ function BuyBattery() {
 	];
 	return (
 		<div id={styles.buybattery}>
-			<div className={styles.batteriesContainer}>
+			<div className={styles.topContainer}>
+				<Header />
 				<Batteries />
 			</div>
 			<div className={styles.logoandtxt}>
-				<img src="src/assets/bambooForestLogo.png" alt="" />
 				<h3>추가할 배터리 수를 선택하세요.</h3>
 			</div>
 			<div className={styles.buybatterylist}>
