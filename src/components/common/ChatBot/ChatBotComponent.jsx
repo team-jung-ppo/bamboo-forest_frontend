f0ee70ee1fd802c9b37ece9346536c2088ba8f2a
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-27 16:16:12 +0900

    💄Modify : ChatBotComponent 스타일 수정

--- src/components/common/ChatBot/ChatBotComponent.jsx
+++ src/components/common/ChatBot/ChatBotComponent.jsx
@@ -3,10 +3,12 @@ import styles from './chatbotcomponent.module.css';
 function ChatBotComponent(props) {
 	return (
 		<div className={styles.container}>
-			<div className={styles.img}>{props.imgUrl}</div>
+			<div className={styles.imgBox}>
+				<img className={styles.img} src={props.imgUrl} alt="" />
+			</div>
 			<div className={styles.name}>{props.name}</div>
 			<div className={styles.description}>{props.description}</div>
-			<div className={styles.cost}>{props.cost}</div>
+			<div className={styles.cost}>{props.cost} 원</div>
 			<button className={styles.buyBtn}>구매하기</button>
 		</div>
 	);
