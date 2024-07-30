c4f05e6d68a1e29cd232a02295577f9a6ac5216f
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-30 16:54:09 +0900

    💄 Add : 구매한 배터리 컴포넌트 인덱스 추가

--- src/components/common/PayedList/PayedListComponent.jsx
+++ src/components/common/PayedList/PayedListComponent.jsx
@@ -3,11 +3,28 @@ import styles from './payedlistcomponent.module.css';
 function PayedListComponent(props) {
 	return (
 		<div className={styles.container}>
-			<div className={styles.productName}>상품명 : {props.productName}</div>
-			<div className={styles.cost}>결제 금액 : {props.cost}</div>
-			<div className={styles.counts}>수량 : {props.count}</div>
-			<div className={styles.provided}>결제 방법 : {props.payments}</div>
-			<div className={styles.payedAt}>결제시각 : {props.payedAt}</div>
+			<div className={styles.productName}>
+				<div className={styles.productNameLabel}>상품명</div>
+				<div className={styles.productNameValue}>{props.productName}</div>
+			</div>
+			<div className={styles.cost}>
+				<div className={styles.costLabel}>결제 금액</div>
+				<div className={styles.costValue}>{props.cost}</div>
+			</div>
+			<div className={styles.counts}>
+				<div className={styles.countsLabel}>수량</div>
+				<div className={styles.countsValue}>{props.count}</div>
+			</div>
+			<div className={styles.provided}>
+				<div className={styles.providedLabel}>결제 방법</div>
+				<div className={styles.providedValue}>{props.payments}</div>
+			</div>
+			<div className={styles.payedAt}>
+				<div className={styles.payedAtLabel}>결제시각</div>
+				<div className={styles.payedAtValue}>
+					{new Date(props.payedAt).toLocaleString()}
+				</div>
+			</div>
 		</div>
 	);
 }
