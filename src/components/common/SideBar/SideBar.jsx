5ca498ce53a9c4d7d957147831a3fbad98d7180e
Hwangyongjin <dswvgw6412@naver.com>
2024-08-04 04:46:45 +0900

    :memo:Feat : 사용 가능한 봇 렌더링

--- src/components/common/SideBar/SideBar.jsx
+++ src/components/common/SideBar/SideBar.jsx
@@ -23,9 +23,9 @@ export function SideBar({ open, onToggleSideBar }) {
         <p className={styles.title}>사용 가능한 상담봇</p>
         <div className={styles.content}>
           {
-            availableBots && availableBots.map((bot, id) => {
-              <AvailableBot id={id} name={bot.chatBotItem.name} url={bot.chatBotItem.url} description={bot.chatBotItem.description} imageUrl={bot.chatBotItem.imageUrl} price={bot.chatBotItem.price} />
-            })
+            availableBots && availableBots.map((bot, id) => (
+              <AvailableBot key={id} id={id} name={bot.chatBotItem.name} url={bot.chatBotItem.url} description={bot.chatBotItem.description} imageUrl={bot.chatBotItem.imageUrl} price={bot.chatBotItem.price} />
+            ))
           }
         </div>
       </div>
