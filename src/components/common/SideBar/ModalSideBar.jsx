4326e31a135b44b7be8efb4e7f2f784d3e5a6416
Hwangyongjin <dswvgw6412@naver.com>
2024-08-04 14:00:18 +0900

    :lipstick:Modify : 코드 스타일 개선

--- src/components/common/SideBar/ModalSideBar.jsx
+++ src/components/common/SideBar/ModalSideBar.jsx
@@ -3,8 +3,11 @@ import styles from "./modalSideBar.module.css";
 import LogoImg from "../../../assets/bambooForestLogo.png";
 import {ModalBackground} from "../Modal/ModalBackground.jsx";
 import {AvailableBot} from "./AvailableBot.jsx";
+import {usePurchaseChatbots} from "../../../hooks/purchaseChatbots/usePurchaseChatbots.js";
 
 export function ModalSideBar({ open, onToggleSideBar }) {
+  const availableBots = usePurchaseChatbots();
+
   return (
     <>
       <ModalBackground open={open} />
@@ -22,11 +25,9 @@ export function ModalSideBar({ open, onToggleSideBar }) {
           </div>
           <p className={styles.title}>사용 가능한 상담봇</p>
           <div className={styles.content}>
-            <AvailableBot/>
-            <AvailableBot/>
-            <AvailableBot/>
-            <AvailableBot/>
-            <AvailableBot/>
+            {availableBots && availableBots.map((bot, id) => (
+              <AvailableBot key={id} id={id} name={bot.chatBotItem.name} url={bot.chatBotItem.url} description={bot.chatBotItem.description} imageUrl={bot.chatBotItem.imageUrl} price={bot.chatBotItem.price} />
+            ))}
           </div>
         </div>
       </div>
