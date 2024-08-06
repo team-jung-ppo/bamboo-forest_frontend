26367d6e6dbccc5d6a4a07bab5338093b0995bc1
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-08-06 22:34:50 +0900

    ✨Modify : 로그아웃 버튼 제거, 한글화

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -2,7 +2,7 @@ import styles from './header.module.css';
 import BoltOutlinedIcon from '@mui/icons-material/BoltOutlined';
 import LinkOutlinedIcon from '@mui/icons-material/LinkOutlined';
 import { useEffect } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { getCookie, removeCookie, setCookie } from '../../../services/cookie';
 import { useState } from 'react';
@@ -17,87 +17,12 @@ function Header() {
 	const onGoMainPage = () => {
 		navigate('/');
 		window.location.reload();
-	}
-
-	const onLogout = async () => {
-		try {
-			const res = await axios.post(
-				`${import.meta.env.VITE_WAS_URL}/api/members/logout`,
-				null,
-				{
-					withCredentials: true,
-					headers: {
-						Authorization: `${accessToken}`,
-					},
-				},
-			);
-			if (res.status === 204) {
-				removeCookie('accessToken');
-				removeCookie('refreshToken');
-				window.location.reload();
-			}
-		} catch (error) {
-			console.log(error);
-		}
 	};
-
-	const fetchUserInfo = async () => {
-		try {
-			const response = await axios.get(
-				`${import.meta.env.VITE_WAS_URL}/api/members/profile`,
-				{
-					withCredentials: true,
-					headers: {
-						Authorization: `${accessToken}`,
-					},
-				},
-			);
-			const userData = response.data;
-			setUserinfo(userData);
-		} catch (error) {
-			if (error.response && error.response.data.code === 'E003') {
-				try {
-					const refreshToken = getCookie('refreshToken');
-					const res = await axios.post(
-						`${import.meta.env.VITE_WAS_URL}/api/members/reissuance`,
-						null,
-						{
-							withCredentials: true,
-							headers: {
-								Authorization: `${refreshToken}`,
-							},
-						},
-					);
-					const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
-						res.data;
-					setCookie('accessToken', newAccessToken);
-					setCookie('refreshToken', newRefreshToken);
-					fetchUserInfo();
-					return;
-				} catch (reissuanceError) {
-					console.error('Error reissuing token', reissuanceError);
-				}
-			}
-			if (axios.isAxiosError(error)) {
-				console.error('Axios error:', error.response?.data || error.message);
-			} else {
-				console.error('Unknown error:', error);
-			}
-		}
-	};
-
-	useEffect(() => {
-		fetchUserInfo();
-	}, []);
 
 	return (
 		<div className={styles.header}>
 			<div className={styles.profile} onClick={onGoMainPage}>
-				<img
-					className={styles.profileImg}
-					src={Logo}
-					alt="profile"
-				/>
+				<img className={styles.profileImg} src={Logo} alt="profile" />
 				<div className={styles.profileInfo}>
 					<h3>대나무숲</h3>
 				</div>
@@ -105,7 +30,7 @@ function Header() {
 			<div className={styles.rightHeader}>
 				<Link to="/paybattery">
 					<div className={styles.buyBattery}>
-						<div className={styles.buyBatteryTxt}>Buy Battery</div>
+						<div className={styles.buyBatteryTxt}>배터리 상점</div>
 						<div className={styles.buyBatteryIcon}>
 							<BoltOutlinedIcon />
 						</div>
@@ -113,7 +38,7 @@ function Header() {
 				</Link>
 				<Link to="/buychatbot">
 					<div className={styles.buyChatbot}>
-						<div className={styles.buyChatbotTxt}>Buy ChatBot</div>
+						<div className={styles.buyChatbotTxt}>챗봇 상점</div>
 						<div className={styles.buyChatbotIcon}>
 							<LinkOutlinedIcon />
 						</div>
@@ -122,17 +47,6 @@ function Header() {
 				{accessToken && (
 					<div className={styles.batteryInfo}>
 						<Batteries />
-					</div>
-				)}
-				{!accessToken ? (
-					<div className={styles.authButton}>
-						<Link to="/login">
-							<button>로그인</button>
-						</Link>
-					</div>
-				) : (
-					<div className={styles.authButton}>
-						<button onClick={onLogout}>로그아웃</button>
 					</div>
 				)}
 			</div>
